6b32e8d01bb3336ac1d9e067676663b1c0bd6b70
woonayoung <nywoo0225@gmail.com>
2020-12-03 23:48:08 +0900

    중간 체크

--- app.js
+++ app.js
@@ -1,1 +1,12 @@
-console.log("ex");
+const testNum = [1, 2, 3, 4, 5];
+
+let result;
+
+result = testNum.reduce((acc, cur, i, ele) => {
+  console.log(ele);
+  console.log(i);
+
+  return acc + cur;
+});
+
+console.log(result);
