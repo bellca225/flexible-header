8d827f6b8ecf36544bc4da2fa789479205bbcd12
woonayoung <nywoo0225@gmail.com>
2020-12-06 20:13:23 +0900

    js 완료

--- app.js
+++ app.js
@@ -1,12 +1,21 @@
-const testNum = [1, 2, 3, 4, 5];
+// const testNum = [1, 2, 3, 4, 5];
 
-let result;
+// let result;
 
-result = testNum.reduce((acc, cur, i, ele) => {
-  console.log(ele);
-  console.log(i);
+// result = testNum.reduce((acc, cur, i, ele) => {
+//   console.log(ele);
+//   console.log(i);
 
-  return acc + cur;
+//   return acc + cur;
+// });
+
+// console.log(result);
+
+const toggleBtn = document.querySelector(".header__toggleBtn");
+const menu = document.querySelector("nav");
+const sns = document.querySelector(".header__sns");
+
+toggleBtn.addEventListener("click", () => {
+  menu.classList.toggle("act"); // 마우스를 클릭했을 때 menu와 sns의 클래스가 active가 있다면 빼주고 없다면 넣어준다.
+  sns.classList.toggle("act");
 });
-
-console.log(result);
