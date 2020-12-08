715815484e25d35f0a2ad534d16f687506cf21d0
woonayoung <nywoo0225@gmail.com>
2020-12-08 23:44:25 +0900

    코드 정리

--- app.js
+++ app.js
@@ -1,16 +1,3 @@
-// const testNum = [1, 2, 3, 4, 5];
-
-// let result;
-
-// result = testNum.reduce((acc, cur, i, ele) => {
-//   console.log(ele);
-//   console.log(i);
-
-//   return acc + cur;
-// });
-
-// console.log(result);
-
 const toggleBtn = document.querySelector(".header__toggleBtn");
 const menu = document.querySelector("nav");
 const sns = document.querySelector(".header__sns");
